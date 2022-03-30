053554a11539fb329961596e888af264e0df3255
Renato <Renato@HANABI>
2022-03-30 01:29:41 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ExtratorValorDeArgumentosURL
+    {
+        private readonly string _argumentos;
+        public string URL { get; }
+
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+            if(string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException("O argumento url não pode ser nulo ou vazio", nameof(url));
+            }
+
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+
+            URL = url;
+        }
+
+        public string GetValor(string nomeParametro)
+        {
+            string termo = nomeParametro + "=";
+            int indiceTermo = _argumentos.IndexOf(termo);
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length);
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if (indiceEComercial == -1)
+            {
+                return resultado;
+            }
+
+            return resultado.Remove(indiceEComercial);
+        }
+    }
+    
+}
---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valorURL = "https://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=Dolar&valor=15000";
+
+            string mensagemOrigem = "PALAVRA";
+            string termoBusca = "ra";
+
+            Console.WriteLine(mensagemOrigem.IndexOf(termoBusca));
+
+            termoBusca = termoBusca.Replace('r', 'R');
+            Console.WriteLine(termoBusca);
+
+            termoBusca = termoBusca.Replace('a','A');
+            Console.WriteLine(termoBusca);
+
+            Console.WriteLine(mensagemOrigem.IndexOf(termoBusca));
+            Console.ReadLine();
+
+
+        }
+    }
+}
